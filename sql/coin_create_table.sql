b3f6c312459f26d93d71800b752c7a03d31910f5
NAMSOO-KIM <golden3333@hanmail.net>
2021-04-05 11:19:18 +0900

    update_sql

--- sql/coin_create_table.sql
+++ sql/coin_create_table.sql
@@ -1,13 +1,4 @@
--- ������ Oracle SQL Developer Data Modeler 20.4.0.374.0801
---   ��ġ:        2021-03-31 11:29:46 KST
---   ����Ʈ:      Oracle Database 11g
---   ����:      Oracle Database 11g
 
-
-
--- predefined type, no DDL - MDSYS.SDO_GEOMETRY
-
--- predefined type, no DDL - XMLTYPE
 DROP TABLE customer CASCADE CONSTRAINTS;
 DROP TABLE product CASCADE CONSTRAINTS;
 DROP TABLE shipment CASCADE CONSTRAINTS;
@@ -15,7 +6,7 @@ DROP TABLE shipment_company CASCADE CONSTRAINTS;
 DROP TABLE orders CASCADE CONSTRAINTS;
 DROP TABLE category CASCADE CONSTRAINTS;
 
--- 시퀀스 삭제
+
 
 drop sequence customer_id_seq;
 drop sequence product_id_seq;
@@ -25,16 +16,12 @@ drop sequence shipment_company_id_seq;
 drop sequence category_id_seq;
 
 
--- shipment_id_idx, shipment_product_id_idx, shipment_shipment_company_id_idx
-
--------------------------------------------------------
 CREATE SEQUENCE customer_id_seq
 START WITH 1
 INCREMENT BY 1
 MAXVALUE 999999
 MINVALUE 1
 NOCYCLE;
---CACHE
 
 CREATE SEQUENCE product_id_seq
 INCREMENT BY 1
@@ -85,17 +72,6 @@ ON category(id);
 ALTER TABLE category
 ADD CONSTRAINT category_id_pk PRIMARY KEY(id);
 
--- 샘플 데이터 삽입
---insert into category values(1,'clothing');
---insert into category values(2,'digital');
---insert into category values(3,'furniture');
---insert into category values(4,'book');
---insert into category values(5,'etc');
---commit;
-
---insert into product values(1,1,'asdf','124',123,1,'의류',1);
-
--------------------------------------------------------
 
 
 
@@ -118,8 +94,6 @@ ADD CONSTRAINT customer_id_pk PRIMARY KEY(id);
 
 
 
-
--------------------------------------------------------
 CREATE TABLE shipment_company (
     id    NUMBER(9) NOT NULL,
     name  VARCHAR2(20 CHAR) NOT NULL
@@ -128,10 +102,7 @@ LOGGING;
 
 ALTER TABLE shipment_company ADD CONSTRAINT shipment_company_id_pk PRIMARY KEY ( id );
 
--- 샘플 데이터
 
-
--------------------------------------------------------
 
 create table PRODUCT (
   id       number(9) NOT NULL,
@@ -140,43 +111,26 @@ create table PRODUCT (
   information       varchar2(200 CHAR),
   price            number(9) NOT NULL,
   category_id      number(9) NOT NULL,
-  category_name    varchar2(50) NOT NULL, --
-  product_status   VARCHAR2(10 CHAR) NOT NULL, --판매 상태 정보 추가 (READY,PROGRESS,FINISH)
+  category_name    varchar2(50) NOT NULL, 
+  product_status   VARCHAR2(10 CHAR) NOT NULL,
   shipment_id      number(9) NOT NULL,
-  buy_customer_id  number(9)   -- 구매자 정보 추가
+  buy_customer_id  number(9)   
   )
 partition by list(category_id)
 (
-  --partition P_CLOTHING    values ('clothing','fashion'),
+  
   partition P_CLOTHING    values (1,2),
-  --partition P_DIGITAL    values ('digital','home appliances'),
+  
   partition P_DIGITAL    values (3,4),
-  --partition P_FURNITURE   values ('furniture','interior'),
+  
   partition P_FURNITURE   values (5,6),
-  --partition P_BOOK    values ('book,ticket'),
+  
   partition P_BOOK    values (7,8),
-  --partition P_ETC    values ('etc','unknown')
+  
   partition P_ETC    values (9,10)
-  --partition P_ETC values (default)
+  
 );
 
-
--- 샘플데이터
--- insert into product values(1,1,'asdf','124',123,1,'의류',1);
-
-"""
-CREATE TABLE product (
-    id           NUMBER(9) NOT NULL,
-    customer_id  NUMBER(9) NOT NULL,
-    name         VARCHAR2(50 CHAR) NOT NULL,
-    information  VARCHAR2(200 CHAR),
-    price        NUMBER(9) NOT NULL,
-    category_id  NUMBER(9) NOT NULL,
-    category_name VARCHAR2(50 CHAR) NOT NULL, -- 새로 추가됨 (카테고리 명)
-	status VARCHAR2(10 CHAR) NOT NULL, --판매 상태 정보 추가 (READY,FINISH)
-    shipment_id  NUMBER(9) NOT NULL
-);
-"""
 
 
 
@@ -184,7 +138,7 @@ CREATE TABLE product (
 CREATE INDEX product_id_idx
 ON product(id);
 
---foreign Ű�� �޾ƿͼ� �ε��� ����
+
 CREATE INDEX product_customer_id_idx
 ON product(customer_id);
 
@@ -207,22 +161,16 @@ ADD CONSTRAINT product_shipment_id_uk UNIQUE(shipment_id);
 ALTER TABLE product
 ADD CONSTRAINT product_category_id_nn CHECK(category_id IS NOT NULL);
 
--- 일단 보류 product_insert 프로시저에서 에러발생(무결성 제약조건 위배)
--- ALTER TABLE product
--- ADD CONSTRAINT product_category_id_fk FOREIGN KEY(category_id)
--- REFERENCES category(id);
-
 ALTER TABLE product
 ADD CONSTRAINT product_customer_id_fk FOREIGN KEY(customer_id)
 REFERENCES customer(id);
 
 
 
-ALTER TABLE PRODUCT ENABLE ROW MOVEMENT; -- 파티션 수정
+ALTER TABLE PRODUCT ENABLE ROW MOVEMENT; 
 
 
 
--------------------------------------------------------
 CREATE TABLE shipment (
     id                      NUMBER(9) NOT NULL,
     shipment_company_id     NUMBER(9) NOT NULL,
@@ -230,7 +178,7 @@ CREATE TABLE shipment (
     product_customer_id     NUMBER(9) NOT NULL
 )
 LOGGING;
---인덱스 shipment_id_idx, shipment_product_id_idx, shipment_shipment_company_id_idx
+
 CREATE INDEX shipment_id_idx
 ON shipment(id);
 
@@ -246,28 +194,11 @@ ALTER TABLE shipment
 ADD CONSTRAINT shipment_product_id_nn
 CHECK (product_id is not null); 
 
--- 1:1 mandetory not null, unique �Ӽ�, fk
+
 
 ALTER TABLE shipment
 ADD CONSTRAINT shipment_product_id UNIQUE(product_id);
 
--- insert shipment에서 무결성 제약조건 걸려서 보류
-
--- ALTER TABLE shipment
--- ADD CONSTRAINT shipment_shipment_company_id_fk 
--- FOREIGN KEY(shipment_company_id)
--- REFERENCES shipment_company(id);
-
---shipment_product_id_fk 무결성 제약조건 걸려서 보류
--- ALTER TABLE shipment
--- ADD CONSTRAINT shipment_product_id_fk FOREIGN KEY (product_id, product_customer_id)
--- REFERENCES product(id, customer_id);
-
-
-------------------------
----해보기------------------------
-
---drop table temp_shipment cascade constraint;
 CREATE TABLE temp_shipment (
     id                      NUMBER(9) NOT NULL,
 
@@ -276,7 +207,7 @@ CREATE TABLE temp_shipment (
     product_customer_id     NUMBER(9) NOT NULL
 )
 LOGGING;
---인덱스 shipment_id_idx, shipment_product_id_idx, shipment_shipment_company_id_idx
+
 CREATE INDEX shipment_id_idx
 ON temp_shipment(id);
 
@@ -292,39 +223,29 @@ ALTER TABLE temp_shipment
 ADD CONSTRAINT shipment_product_id_nn
 CHECK (product_id is not null); 
 
--- 1:1 mandetory not null, unique �Ӽ�, fk
 
 ALTER TABLE temp_shipment
 ADD CONSTRAINT shipment_product_id UNIQUE(product_id);
 
--------------------------------------------------------------------
-
-------------------------
--- 클러스터 생성
 
 create cluster shipment_clu
 (shipment_id number(9))
 size 1k
 tablespace users;
 
--- 클러스터용 인덱스 생성
+
 create index shipment_index
 on cluster shipment_clu;
 
 
 
--- 테이블을 클러스터 안에 생성
+
 create table shipment
 cluster shipment_clu(id)
 as
 select * from temp_shipment;
 commit;
 
--- 이름 바꾸기
---rename temp_shipment to old_shipment;
---rename shipment to temp_shipment;
-
--- 테이블에 추가 인덱스 생성
 create index shipment_id_clu_idx
 on shipment(id);
 
@@ -335,15 +256,13 @@ on shipment(id);
 
 
 
--------------------------------------------------------
--- product ���̺��� ���� �������� �߰�
+
 ALTER TABLE product
 ADD CONSTRAINT product_shipment_id_fk
 FOREIGN KEY(shipment_id) REFERENCES shipment(id)
 deferrable initially deferred;
 
--------------------------------------------------------
--- ORDER
+
 
 
 CREATE TABLE ORDERS (
@@ -367,16 +286,7 @@ ON ORDERS(customer_id);
 CREATE INDEX ORDERS_product_id_idx
 ON ORDERS(product_id);
 
--- ALTER TABLE ORDERS
--- ADD CONSTRAINT ORDERS_id_pk PRIMARY KEY(id);
 
-"""
-이부분 복합키에 외래키가 포함되도록..
-
-
-외래키만 지정할 경우. 식별 값이 없어도 테이블에 삽입이 가능해서.
-
-"""
 ALTER TABLE ORDERS
 ADD CONSTRAINT ORDERS_customer_buyer_id_fk FOREIGN KEY(customer_id)
 REFERENCES customer(id);
@@ -387,7 +297,7 @@ REFERENCES product(id, customer_id);
 
 
 
--- 트리거 등록
+
 CREATE OR REPLACE TRIGGER TRIG_TEST
 		AFTER UPDATE OF PRODUCT_STATUS ON PRODUCT   
 		FOR EACH ROW
@@ -419,16 +329,7 @@ CREATE OR REPLACE TRIGGER TRIG_TEST
 	END;
 	
 	
--- 뷰 정의 (전체 물품 목록 조회)
-"
-권한 필요할 때 사용
-sqlplus system/oracle@localhost:1521/xepdb1
 
-alter user coin
-quota unlimited on users;
-
-grant create any table to coin;
-"
 create or replace view all_product_view
 AS
 SELECT p.id, p.name, p.information, p.price, c.name as product_custmoer_name, p.category_name, p.product_status, sp.name as ship_company_name, (select name from customer where id= p.buy_customer_id) as buy_custmoer_name
--- sql/plsql.sql
+++ sql/plsql.sql
@@ -1,50 +1,4 @@
--- PL/SQL
 
--- ctrl + f 로 해당 프로시저 검색
-
--- 회원가입----------------------------------
--- 고객 아이디 중복 체크 프로시저
--- 회원 가입 프로시저
-
--- 로그인 ----------------------------------
--- 로그인 프로시저
--- customer 정보 받아오기
-
--- 메인 페이지 --------------------------------
--- 전체 글 조회 프로시저
--- 카테고리 별 조회 프로시저
--- 제품 상세정보
--- 카테고리 별 조회
--- 물품 구매 (마일리지 차감) --(04-04 09:08 업데이트)
-
-
--- 마이페이지 -------------------------------
--- 내가 올린 상품 보기 
--- 물품 등록 		
--- 수령 확인 버튼- 업데이트 09:55
--- 카테고리 리스트 박스
--- 배송회사 리스트 박스
--- 내가 판매중인 물품
--- 내가 구매중인 물품
-
--- 차트 페이지 --------------------------
--- 전체 상품 마일리지 랭킹
-
-
------------------------------------------------------------------------------
---                          회원가입                                        --
------------------------------------------------------------------------------
-
--- 고객 아이디 중복 체크 프로시저
--- 회원가입 프로시저
-
-"""
--- 중복 체크만 해당
-1. OUTPUT PARAM을 이용해 APP에 결과 통보
-2. 1 이면 중복 - 가입 불가
-3. 0 이면 중복X - 가입가능
-"""
--- 아이디 중복 체크
 CREATE OR REPLACE PROCEDURE C_NAMECHECK
 (
   c_name IN customer.name%TYPE,
@@ -59,14 +13,7 @@ BEGIN
 END;
 /
 
--- 실행
-VAR R NUMBER;
-EXEC C_NAMECHECK('asdf',:R);
-print R
 
------------------------------------------------------------------------------
-
--- 회원 가입 프로시저
 
 CREATE OR REPLACE PROCEDURE customer_insert_version2
 (
@@ -81,13 +28,13 @@ CREATE OR REPLACE PROCEDURE customer_insert_version2
 IS
   MR NUMBER;
 BEGIN
-  -- 중복체크
+ 
   C_NAMECHECK(name, MR);
   IF MR = 0 THEN
-    --테이블에 데이터 넣기
+   
     INSERT INTO CUSTOMER(id,name,password,zipcode,phone_number,coin, volunteer_working_time)
 	VALUES(customer_id_seq.nextval,name, password, zipcode, phone_number, coin,  volunteer_working_time);
-    possible := 1 ; --possible 반대,
+    possible := 1 ; 
     COMMIT;
   ELSE
     possible := 0 ;
@@ -96,17 +43,7 @@ BEGIN
 END ;
 /
 
--- 실행
-VAR R NUMBER;
-EXEC customer_insert_version2('sadgs','cabw','asdfaseg','1231',1241,132,:R);
-PRINT R;  -- 1 리턴되면 회원가입 성공, 0일때 회원가입 실패
 
-
-
------------------------------------------------------------------------------
---                          로그인                                        --
------------------------------------------------------------------------------
--- 로그인 프로시저
 CREATE OR REPLACE PROCEDURE customer_login_check
 (
   c_name IN customer.name%TYPE,
@@ -124,7 +61,7 @@ END;
 
 CREATE OR REPLACE PROCEDURE customer_login
 (
-    --id IN customer.id%TYPE,
+    
 	name IN customer.name%TYPE,
 	password IN customer.password%TYPE,
     possible OUT NUMBER
@@ -132,28 +69,22 @@ CREATE OR REPLACE PROCEDURE customer_login
 IS
   MR NUMBER;
 BEGIN
-  -- 중복체크
+  
   customer_login_check(name,password, MR);
   IF MR = 0 THEN
 	
-    possible := 0 ; -- 0이면 등록된 아이디가 없거나 비밀번호가 틀렸습니다.
+    possible := 0 ; 
 	
   ELSE
   
-    possible := 1 ; -- 1이면 로그인 성공
+    possible := 1 ;
 	
   END IF;
   DBMS_OUTPUT.PUT_LINE(TO_CHAR(MR));
 END ;
 /
 
--- 실행
-VAR R NUMBER;
-EXEC customer_login('id','pw',:R);
-PRINT R; -- 1이 로그인 성공!, 0은 로그인 실패
 
-------------------------------------------------------------------------
--- customer 정보 받아오기
 
 CREATE OR REPLACE procedure sp_get_customer_info ( c_name IN customer.name%TYPE, record_list OUT SYS_REFCURSOR )
 AS
@@ -166,26 +97,6 @@ END;
 /
 
 
------------------------------------------------------------------------------
------------------------------------------------------------------------------
---                          메인 페이지                                     --
------------------------------------------------------------------------------
-
-
--- 모든 제품 조회(구매자 포함) --4/5 (02:09 업데이트)
--- 전체 글 조회 프로시저
--- 카테고리 별 조회 프로시저
--- 제품 상세정보
--- 카테고리 별 조회--
--- 물품 구매( 마일리지 차감)
--- 모든 제품 조회(구매자 포함) --4/5 (02:09 업데이트)
--- 물품 구매 ( 마일리지 차감) 트리거로
--- 물품 구매( 마일리지 차감) - 수정전 (구버전)
--- 물품 구매( 마일리지 차감) - 수정 후(최신버전)
-----------------------------------------------------------------------
-----------------------------------------------------------------------
-
--- 모든 제품 조회(구매자 포함) --4/5 (02:09 업데이트)
 CREATE OR REPLACE procedure ALL_product
 (
 ALL_PRODUCT_record  OUT SYS_REFCURSOR
@@ -200,13 +111,7 @@ INNER JOIN shipment s on p.shipment_id = s.id
 INNER JOIN shipment_company sp on sp.id = s.shipment_company_id;
 END;
 /
-"
-var pro_detail refcursor;
-exec ALL_product(:pro_detail);
-print pro_detail;
-"
 
--- 모든 제품 뷰를 조회(구매자 포함) -- 4/5 (10:32 업데이트)
 CREATE OR REPLACE procedure all_product_view_select
 (
 	all_product_view_record  OUT 		SYS_REFCURSOR
@@ -219,16 +124,7 @@ BEGIN
 END;
 /
 
-"
-var p_all_ref refcursor;
-exec all_product_view_select(:p_all_ref);
-print p_all_ref;
-"
 
-
-----------------------------------------------------------------------
-
--- 전체 글 조회
 CREATE OR REPLACE procedure select_productListAll
 (
 	productList_record  OUT 		SYS_REFCURSOR
@@ -241,14 +137,10 @@ FROM product;
 END;
 /
 
--- 실행 확인
 var p_all refcursor;
 exec select_productListAll(:p_all);
 print p_all;
 
-------------------------------------------------------------------------
-
--- 카테고리 별 조회--
 
 CREATE OR REPLACE PROCEDURE select_category
 (
@@ -278,8 +170,6 @@ print category_select;
 
 
 
------------------------------------------------------------------------------
--- 제품 상세정보
 
 CREATE OR REPLACE procedure product_detail
 (
@@ -295,74 +185,7 @@ BEGIN
 END;
 /
 
---실행
-var pro_detail refcursor;
-exec product_detail(1,:pro_datail)
-print pro_detail;
 
-------------------------------------------------------------------------------------------
-"
--- 물품 구매( 마일리지 차감) - 수정전 (구버전)
-CREATE OR REPLACE PROCEDURE buy_item
-(
-	
-	p_id IN product.id%Type,
-	p_price IN product.price%Type,
-    c_id IN customer.id%TYPE,
-	update_coin IN customer.coin%Type
-)	
-IS
-BEGIN
-	
-	update product set PRODUCT_STATUS = 'PROGRESS' where p_id = id;
-	update customer set coin = update_coin where c_id = id;
-	COMMIT;
-END ;
-/
-exec buy_item(1,1,1,777);
-"
------------------------------------------------------------------------------------------------
-"
--- 물품 구매( 마일리지 차감) - 수정 후(최신버전)
-CREATE OR REPLACE PROCEDURE buy_item
-(
-	
-	p_id IN product.id%Type
-	
-)	
-IS
-BEGIN
-	
-	update product set PRODUCT_STATUS = 'PROGRESS' where p_id = id;
-	COMMIT;
-END ;
-/
-"
-
-------------------------------------------------------------------------------------------------------
-
-
------------------------------------------------------------------------------
-
------------------------------------------------------------------------------
---                          마이 페이지                                     --
------------------------------------------------------------------------------
-
--- 내가 올린 상품 보기 
--- 물품 등록 		
--- 수령 확인 버튼- 업데이트 09:55
--- 카테고리 리스트 박스
--- 배송회사 리스트 박스
--- 내가 판매중인 물품
--- 내가 구매중인 물품
-
--- 상품 수정 (업데이트 19:46)
--- 판매 대기중인 것만 삭제(업데이트 20:48)
--- 물품 구매(대기상태 = 'READY'만 거래가능) - 4/4 23:03 업데이트
--- 수령 확인 트리거 (4:00 업데이트)
-
---------------------------------------------------------------------------------
--- 내가 올린 상품 보기
 CREATE OR REPLACE procedure customer_sell_product
 (
 	c_id 			IN 		customer.id%TYPE,
@@ -377,23 +200,15 @@ BEGIN
 END;
 /
 
---실행
-var category_select refcursor;
-exec customer_sell_product(1,:category_select)
-print category_select;
-
-------------------------------------------------------------------------------------
--- 물품 등록
-
 CREATE OR REPLACE PROCEDURE product_insert
 (
 
-	customer_id IN customer.id%TYPE, -- 고객 id
-    p_name IN product.name%Type, -- 제품이름
-	information IN product.information%TYPE, --제품 설명
-	price IN product.price%TYPE, -- 제품 가격
-    category_name IN category.name%TYPE, --카테고리 이름
-	shipment_name IN shipment_company.name%TYPE -- 배송회사 이름
+	customer_id IN customer.id%TYPE, 
+    p_name IN product.name%Type, 
+	information IN product.information%TYPE, 
+	price IN product.price%TYPE, 
+    category_name IN category.name%TYPE,
+	shipment_name IN shipment_company.name%TYPE
 	
 )
 IS 
@@ -404,7 +219,7 @@ IS
 	
 BEGIN
 
-	--- category 아이디 변수로 저장
+
 	SELECT id INTO category_id
 	FROM category
 	where name=category_name;
@@ -414,15 +229,10 @@ BEGIN
 	where name=shipment_name;
 	
 	
-	
-	-- product 테이블에 저장
-	
 	insert into product(id,customer_id,name,information,price,category_id,category_name,product_status,shipment_id)
 	values(product_id,customer_id,p_name, information, price,
 	category_id,category_name, 'READY', ship_id);
-	
-	-- product customer id 판매자
-	
+
 	
 	insert into shipment(id,shipment_company_id,product_id, PRODUCT_CUSTOMER_ID)
 	values(ship_id, shipcom_id, product_id, customer_id);
@@ -432,19 +242,13 @@ BEGIN
 END; 
 /
 
-exec product_insert(1,'충전기 또 팔아연', '좋아요 이거', 1000, 'clothing','hangin');
-
---------------------------------------------------------------------------
-
---물품 구매(대기상태 = 'READY'만 거래가능) - 4/4 04:28 업데이트
-
-CREATE OR REPLACE PROCEDURE buy_item -- 최신버전
+CREATE OR REPLACE PROCEDURE buy_item 
 (
    
    p_id IN product.id%Type, 
-   product_customer_name IN customer.name%Type, -- 판매자 id값
+   product_customer_name IN customer.name%Type, 
    c_id IN customer.id%TYPE,
-   contract_date IN orders.contract_date%Type, -- 현재 날짜도 넣어줘.
+   contract_date IN orders.contract_date%Type, 
    
    ispossible OUT NUMBER
 )   
@@ -479,25 +283,14 @@ BEGIN
 END ;
 /
 
---VAR POSSIBLE NUMBER;
---EXEC buy_item(1,2,4,'21/03/25',1400,:POSSIBLE);
---PRINT POSSIBLE;
-------------------------------------------
-
-----------------------------------------------------------
---수령 확인 트리거 (4:00 업데이트)
--- 수령 확인 누르면 판매자에게 금액 전달 + 거래성사금액 100마일리지 지급. 구매자도 거래성사 시 100마일리지 지급
 CREATE OR REPLACE TRIGGER TRIG_TEST
-	AFTER UPDATE OF PRODUCT_STATUS ON PRODUCT   --컬럼명 ON 테이블 명
+	AFTER UPDATE OF PRODUCT_STATUS ON PRODUCT  
 	FOR EACH ROW
 DECLARE
---p_status product.PRODUCT_STATUS%TYPE;
+
 
 BEGIN
 
-	-- 1. 확인 버튼 누르면 상태값(progress -> finish) 변경
-	
-	-- 2. 마일리지를 판매자에게 지급. 거래 성사되어 마일리지 양쪽에 100씩 지급
 	
 	IF :NEW.PRODUCT_STATUS = 'FINISH' THEN
 		UPDATE CUSTOMER
@@ -523,17 +316,9 @@ END;
 /
 
 
---실행
-
-update product set PRODUCT_STATUS = 'PROGRESS' where id = 3;
-update product set PRODUCT_STATUS = 'FINISH' where id = 3;
-
--- 공사중 ....
--- 수령확인 버튼 클릭 시 이벤트
----------------------------------------------------------------
 CREATE OR REPLACE PROCEDURE receipt_click
 (
- -- 제품 id 받아서 상태만 변경
+
  
    p_id IN product.id%TYPE
    
@@ -552,14 +337,6 @@ END;
 
 
 
-
-
-
-----------------------------------------------------
--------------------------------------------------------
---------------------------------------------------------
--- 카테고리 리스트 박스
-
 CREATE OR REPLACE procedure category_allbox
 (
 	category_list_record  OUT 		SYS_REFCURSOR
@@ -572,13 +349,6 @@ BEGIN
 END;
 /
 
-var category_all refcursor;
-exec category_allbox(:category_all);
-print category_all;
-
-
---------------------------------------------------------
--- 배송회사 리스트 박스
 
 CREATE OR REPLACE procedure shipment_company_allbox
 (
@@ -598,23 +368,17 @@ print ship_com_all;
 
 
 
-
-
---------------------------------------------------
--- 내가 판매중인 물품
-
-/* 등록 물품 수정 */
-ALTER TABLE PRODUCT ENABLE ROW MOVEMENT; -- 파티션 수정
+ALTER TABLE PRODUCT ENABLE ROW MOVEMENT; 
 
 CREATE OR REPLACE PROCEDURE mysell_product_update
 (
    p_id IN product.id%TYPE,
-   customer_id IN customer.id%TYPE, -- 고객 id
-    p_name IN product.name%Type, -- 제품이름
-   P_information IN product.information%TYPE, --제품 설명
-   p_price IN product.price%TYPE, -- 제품 가격
-    p_category_name IN category.name%TYPE, --카테고리 이름
-   shipment_name IN shipment_company.name%TYPE -- 배송회사 이름
+   customer_id IN customer.id%TYPE, 
+    p_name IN product.name%Type,
+   P_information IN product.information%TYPE, 
+   p_price IN product.price%TYPE, 
+    p_category_name IN category.name%TYPE, 
+   shipment_name IN shipment_company.name%TYPE 
    
 )
 IS 
@@ -647,12 +411,10 @@ END;
 /
 
 
----------------------------------------------------------------------------
--- 내가 구매중인 물품
 CREATE OR REPLACE PROCEDURE mybuy_product
 (
 
-	c_id IN customer.id%TYPE, -- 고객 id
+	c_id IN customer.id%TYPE, 
 	mybuy_list_record OUT SYS_REFCURSOR
 	
 )
@@ -671,8 +433,6 @@ var s refcursor;
 exec mybuy_product(1,:s);
 print s;
 
----------------------------------------------------------------------------------
--- 판매 대기중인 것만 삭제
 CREATE OR REPLACE PROCEDURE mysell_product_cancel
 (
 	p_id IN product.id%TYPE
@@ -680,8 +440,7 @@ CREATE OR REPLACE PROCEDURE mysell_product_cancel
 )
 IS 	
 BEGIN
-	-- 판매 취소
-	
+
 	
 	DELETE FROM product
 	where p_id = id ;
@@ -693,21 +452,6 @@ BEGIN
 	commit;
 END; 
 /
-
--- 실행
-
------------------------------------------------------------------------------------ 
--- 상품 수정
-
---------------------------------------------------------------------
-
-
------------------------------------------------------------------------------
------------------------------------------------------------------------------
---                          차트 페이지                                     --
------------------------------------------------------------------------------
-
--- 전체 상품 마일리지 랭킹
 
 CREATE OR REPLACE procedure select_coinRanking
 (
@@ -721,12 +465,3 @@ FROM customer c
 ORDER BY c.coin DESC;
 END;
 /
-
--- 실행
-var c refcursor;
-exec select_coinRanking(:c);
-print c
-
-
----------------------------------------------------------------------------------
-
